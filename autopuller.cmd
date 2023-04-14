7ab8b6595fa095a3c4360b273cc7c2d257080667
KaO <thierryoliv@gmail.com>
2023-04-14 18:50:37 +0200

    1st commit

---
+++ autopuller.cmd
@@ -0,0 +1,14 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set /p root_folder=<root_folder.txt
+
+for /d %%D in ("%root_folder%\*") do (
+  echo Traitement du dossier %%~nxD...
+  pushd "%%D"
+  ::git pull
+  popd
+)
+
+endlocal
+pause
